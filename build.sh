b75153a9ac3158df4e522d9a22cec6e9aab57c56
Aaron Maturen <aaron.maturen@gmail.com>
2012-05-21 22:11:47 -0400

    added support to save keywords to resource_data

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# get current commit
+git log | grep -m 1 commit
+commit=`git log | grep -m 1 commit `
+commitnick=${commit:7:10}
+commitnick='0.3'
+echo "updating version information in inline_keywords.yaml with latest git commit $commitnick"
+sed -ibk "s/version: .*/version: $commitnick/" inline_keywords.yaml
+rm inline_keywords.yamlbk
+cd ..
+rm inline_keywords*.rsp
+tar -cz -f inline_keywords-$commitnick.rsp -X inline_keywords/exclude.txt inline_keywords
